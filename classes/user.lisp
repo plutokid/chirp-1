f1bdfd84097baa2a237fcaf3c0b8304e3987659c
Matt Novenstern <fisxoj@gmail.com>
2014-11-04 19:58:38 -0500

    Add unique constraint on usernames

--- classes/user.lisp
+++ classes/user.lisp
@@ -6,38 +6,51 @@
        :db-constraints (:not-null)
        :reader id)
    (username :type string
-	     :db-constraints :not-null
+	     :db-constraints (:not-null :unique)
 	     :accessor username
 	     :initarg :username)
    (email :type string
 	  :db-constraints :not-null
 	  :accessor email
 	  :initarg :email)
-   (password :type string
-	     :db-constraints :not-null
-	     :accessor password
-	     :initarg :password)
+   (password-digest :type string
+		    :db-constraints :not-null
+		    :accessor password-digest
+		    :initarg :password-digest)
    (chirps :type chirp
 	   :db-kind :join
 	   :db-info (:join-class chirp
 		     :foreign-key user-id
 		     :home-key id)
 	   :accessor chirps))
-  (:base-table users))
+  (:base-table users)
+  (:extra-initargs '(:password)))
+
+(export '(username id email chirps user-url))
 
 ;(clsql:locally-enable-sql-reader-syntax)
 (file-enable-sql-reader-syntax)
 
+(defmethod initialize-instance :after ((user user) &rest initargs &key password &allow-other-keys)
+  (declare (ignore initargs))
+  (unless (slot-boundp user 'id)
+    (setf (password user) password)))
+
+(defmethod (setf password) (password (user user))
+  (setf (password-digest user) (hash-password password)))
+
 (defun user-url (user)
-  (format nil "/user/~d"
-	  (typecase user
-	    (user (id user))
-	    (number user))))
+  (format nil "/users/~d" (username user)))
 
 (defun find-user-by-username (username)
   (first (select 'user
 		 :where [= [slot-value 'user 'username] username]
 		 :flatp t)))
+
+(defun find-user-by-credentials (username password)
+  (let ((user (find-user-by-username username)))
+    (when (check-password password (password-digest user))
+      user)))
 
 (defun find-chirps-by-username (username)
   (chirps (find-user-by-username username)))
